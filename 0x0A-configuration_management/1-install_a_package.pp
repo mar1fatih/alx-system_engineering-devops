cbe09da85971f7689bf189d68cef61400af6f7a3
mar1fatih <marouanefatih631@gmail.com>
2024-01-20 07:24:14 -0800

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
-# install flask from pip3
-package { 'flask':
+#!/usr/bin/pup
+# Install a version of flask (2.1.0)
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }
+
