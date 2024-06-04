bbe4192f36142d9fc4f3ef20a057f8945815489d
mar1fatih <marouanefatih631@gmail.com>
2024-06-04 19:48:53 +0100

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-'''
-module queries and returns the number of subscribers
-'''
-
+""" the number of subscribers """
 import requests
 
 
-def number_of_subscribers(subscribers):
-    '''function that queries and returns subscribers number'''
-    headers = {'User-agent': 'test'}
-    res = requests.get("https://www.reddit.com/r/{}/about.json"
-                       .format(subscribers), headers=headers)
-    try:
-        subs = res.json()['data']['subscribers']
-    except Exception:
+def number_of_subscribers(subreddit):
+    """ Return the total number of subscribers """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+
+    headers = {
+            'User-Agent': "Mozilla/5.0"
+            }
+    res = requests.get(url, headers=headers, allow_redirects=False)
+
+    if res.status_code != 200 or res.json()['data'] is None:
         return 0
-    return subs
+    else:
+        subs = res.json()['data']['subscribers']
+        return subs
