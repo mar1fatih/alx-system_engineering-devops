c4a268ab4ba70eb4dad6c7f77452a44fe31cc54b
mar1fatih <marouanefatih631@gmail.com>
2024-06-04 19:18:30 +0100

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,14 +4,14 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    """ Return the total number of subscribers """
     url = 'https://www.reddit.com' +\
           '/r/{}/about.json'.format(subreddit)
 
     headers = {
             'User-Agent': "Mozilla/5.0"
             }
-    res = requests.get(url, headers=headers,
-                       allow_redirects=False)
+    res = requests.get(url, headers=headers)
 
     if res.status_code != 200:
         return 0
